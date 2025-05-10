bd74fc321cdfcd8d69cc795cf9c5439c4be45b2f
HendEl-Mahdy <hendm8042@gmail.com>
2025-05-10 06:16:40 -0700

    chore: initial project setup for iOS Unit Testing by Example

---
+++ UnitTest_By_Examples/UnitTest_By_ExamplesTests/Ch1_AssertYourselfTests/AssertYourselfTests.swift
@@ -0,0 +1,13 @@
+//
+//  AssertYourselfTests.swift
+//  UnitTest_By_ExamplesTests
+//
+//  Created by Hend El Mahdy on 09/05/2025.
+//
+
+import XCTest
+@testable import UnitTest_By_Examples
+
+final class AssertYourselfTests: XCTestCase {
+   
+}
